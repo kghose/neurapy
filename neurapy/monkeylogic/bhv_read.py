fa0015109ee34e9600e9554582a9d6751ab29873
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-05-06 17:58:47 -0400

    bhv_read now handles .bhv files from version 1.5 onwards

--- neurapy/monkeylogic/bhv_read.py
+++ neurapy/monkeylogic/bhv_read.py
@@ -35,55 +35,66 @@ def read_header(bhv, fin):
   bhv['MagicNumber'] = mn
   bhv['FileHeader'] = fh
   bhv['FileVersion'] = fv
+  logger.info('File version {:1.2f}'.format(fv))
 
-  if fv < 2.72:
+  if fv < 1.5:
     logger.error('File ' + fin.name + 'version is ' + str(fv) + '. ' + __name__ + ' only handles versions greater than 2.72')
     return False
 
-  st, en, inv, sn, cn, cf = unpack('=32s128s128s128s128s128s', fin)
+  st, en = unpack('=32s128s', fin)
   bhv['StartTime'] = st
   bhv['ExperimentName'] = en
-  bhv['Investigator'] = inv
-  bhv['SubjectName'] = sn
-  bhv['ComputerName'] = cn
-  bhv['ConditionsFile'] = cf
+  if fv > 1.5:
+    bhv['Investigator'], = unpack('128s', fin)
+  bhv['SubjectName'] = unpack('128s', fin)
+  if fv > 2.1:
+    bhv['ComputerName'], = unpack('128s', fin)
+  bhv['ConditionsFile'], = unpack('128s', fin)
 
   nc, opc = unpack('HH', fin)
   to = [[unpack('64s', fin)[0] for n in xrange(nc)] for m in xrange(opc)]
-  tfc = [unpack('128s', fin)[0] for n in xrange(nc)]
   bhv['NumConds'] = nc
   bhv['ObjectsPerCond'] = opc
   bhv['TaskObject'] = to
-  bhv['TimingFileByCond'] = tfc
 
+  if fv >= 2.65:
+    bhv['TimingFileByCond'] = [unpack('128s', fin)[0] for n in xrange(nc)]
+    if fv >=2.71:
+      maxblks, = unpack('B', fin)
+      bbc = [[unpack('B', fin)[0] for n in xrange(nc)] for m in xrange(maxblks)]
+    else:
+      bbc = [unpack('B', fin)[0] for n in xrange(nc)]
+    ibc = [unpack('128s', fin)[0] for n in xrange(nc)]
+    bhv['BlockByCond'] = bbc
+    bhv['InfoByCond'] = ibc
 
-  maxblks, = unpack('B', fin)
-  bbc = [[unpack('B', fin)[0] for n in xrange(nc)] for m in xrange(maxblks)]
-  ibc = [unpack('128s', fin)[0] for n in xrange(nc)]
   ntf, = unpack('B',fin)
   tf = [unpack('128s', fin)[0] for n in xrange(ntf)]
-  bhv['BlockByCond'] = bbc
-  bhv['InfoByCond'] = ibc
   bhv['NumTimingFiles'] = ntf
   bhv['TimingFiles'] = tf
 
-
-  el, bl, cd, bsf, csf, vrr, vbp, sxr, syr, vd, ppd, ait, aif, aid = unpack('=64s64s64s64s64sdHHHdd32sd32s', fin)
+  el, bl, cd, bsf, csf = unpack('=64s64s64s64s64s', fin)
   bhv['ErrorLogic'] = el
   bhv['BlockLogic'] = bl
   bhv['CondLogic'] = cd
   bhv['BlockSelectFunction'] = bsf
   bhv['CondSelectFunction'] = csf
-  bhv['VideoRefreshRate'] = vrr
-  bhv['VideoBufferPages'] = vbp
+  if fv>2.0:
+    vrr, vbp = unpack('=dH', fin)
+    bhv['VideoRefreshRate'] = vrr
+    bhv['VideoBufferPages'] = vbp
+
+  sxr, syr, vd, ppd = unpack('=HHdd', fin)
   bhv['ScreenXresolution'] = sxr
   bhv['ScreenYresolution'] = syr
   bhv['ViewingDistance'] = vd
   bhv['PixelsPerDegree'] = ppd
-  bhv['AnalogInputType'] = ait
-  bhv['AnalogInputFrequency'] = aif
-  bhv['AnalogInputDuplication'] = aid
-
+  if fv > 2.01:
+    ait, aif = unpack('=32sd', fin)
+    bhv['AnalogInputType'] = ait
+    bhv['AnalogInputFrequency'] = aif
+  if fv > 2.0:
+    bhv['AnalogInputDuplication'], = unpack('32s', fin)
 
   escm, tmatrix = unpack('32sB', fin)
   et = {}
@@ -102,16 +113,18 @@ def read_header(bhv, fin):
   if tmatrix:
     et['ndims in'], et['ndims out'], et['fwdfcn'], et['invfcn'] = unpack('=HH64s64s', fin)
     tsize, = unpack('H', fin)
-    tsqrt = tsize ** 0.5
+    tsqrt = int(tsize ** 0.5)
     et['tdata T'] = [[unpack('d', fin)[0] for n in xrange(tsqrt)] for m in xrange(tsqrt)]
     et['tdate Tinv'] = [[unpack('d', fin)[0] for n in xrange(tsqrt)] for m in xrange(tsqrt)]
   bhv['JoystickCalibrationMethod'] = jcm
   bhv['JoyTransform'] = et
 
   bhv['PhotoDiodePosition'], = unpack('12s', fin)
-  bhv['ScreenBackgroundColor'] = unpack('3d', fin)
-  bhv['EyeTraceColor'] = unpack('3d', fin)
-  bhv['JoyTraceColor'] = unpack('3d', fin)
+
+  if fv >= 1.9:
+    bhv['ScreenBackgroundColor'] = unpack('3d', fin)
+    bhv['EyeTraceColor'] = unpack('3d', fin)
+    bhv['JoyTraceColor'] = unpack('3d', fin)
 
   np, = unpack('H', fin)
   pic = [{} for n in xrange(np)]
@@ -127,21 +140,22 @@ def read_header(bhv, fin):
     'Pic': pic
   }
 
-  nm, = unpack('H', fin)
-  mov = [{} for n in xrange(nm)]
-  for n in xrange(nm):
-    mov[n]['Name'], = unpack('128s', fin)
-  for n in xrange(nm):
-    mov[n]['Size'], = unpack('3H', fin)
-    mov[n]['NumFrames'], = unpack('H', fin)
-  for n in xrange(nm):
-    sz = mov[n]['Size']
-    numframes = mov[n]['NumFrames']
-    mov[n]['Data'] = []
-    for fr in xrange(numframes):
-      mov[n]['Data'].append(pylab.array([[unpack(sz[0]*'B', fin) for j in xrange(sz[1])] for k in xrange(sz[2])]).T)
-  bhv['Stimuli']['NumMovs'] = nm
-  bhv['Stimuli']['Mov'] = mov
+  if fv >= 2.5:
+    nm, = unpack('H', fin)
+    mov = [{} for n in xrange(nm)]
+    for n in xrange(nm):
+      mov[n]['Name'], = unpack('128s', fin)
+    for n in xrange(nm):
+      mov[n]['Size'], = unpack('3H', fin)
+      mov[n]['NumFrames'], = unpack('H', fin)
+    for n in xrange(nm):
+      sz = mov[n]['Size']
+      numframes = mov[n]['NumFrames']
+      mov[n]['Data'] = []
+      for fr in xrange(numframes):
+        mov[n]['Data'].append(pylab.array([[unpack(sz[0]*'B', fin) for j in xrange(sz[1])] for k in xrange(sz[2])]).T)
+    bhv['Stimuli']['NumMovs'] = nm
+    bhv['Stimuli']['Mov'] = mov
 
   logger.info('Read header (' + str(fin.tell()) + ' bytes)')
   return True
@@ -157,6 +171,8 @@ def read_trials(bhv, fin):
     False otherwise
     bhv is filled in implicitly
   """
+  fv = bhv['FileVersion']
+
   bhv['Padding'] = unpack('1024B', fin)
   bhv['NumTrials'], = unpack('H', fin)
   nTrials = bhv['NumTrials']
@@ -188,65 +204,91 @@ def read_trials(bhv, fin):
   usrvars = [None]*nTrials
 
   for trl in xrange(nTrials):
-    tno[trl], numc = unpack('HB', fin)
-    atst[trl] = unpack('d'*numc, fin)
-    bn[trl], cn[trl], te[trl], cr[trl], mcr[trl], ncodes[trl] = unpack('6H', fin)
+    tno[trl], = unpack('H', fin)
+    if fv > 2.2:
+      numc, = unpack('B', fin)
+      atst[trl] = unpack('d'*numc, fin)
+
+    bn[trl], cn[trl], te[trl] = unpack('3H', fin)
+    if fv >=2.05:
+      cr[trl], = unpack('H', fin)
+      if fv >= 2.72:
+        mcr[trl], = unpack('H', fin)
+
+    ncodes[trl], = unpack('H', fin)
     coden[trl] = unpack('H'*ncodes[trl], fin)
     codet[trl] = unpack('H'*ncodes[trl], fin)
 
-    nxeyep, = unpack('I', fin)
-    xeye[trl] = pylab.array(unpack('f'*nxeyep, fin), dtype=pylab.float16)
-    nyeyep, = unpack('I', fin)
-    yeye[trl] = pylab.array(unpack('f'*nyeyep, fin), dtype=pylab.float16)
+    if fv > 1.5:
+      if fv > 1.6:
+        fmt='f'
+      else:
+        fmt='d'
 
-    nxjoyp, = unpack('I', fin)
-    xjoy[trl] = pylab.array(unpack('f'*nxjoyp, fin), dtype=pylab.float16)
-    nyjoyp, = unpack('I', fin)
-    yjoy[trl] = pylab.array(unpack('f'*nyjoyp, fin), dtype=pylab.float16)
+      nxeyep, = unpack('I', fin)
+      xeye[trl] = pylab.array(unpack(fmt*nxeyep, fin), dtype=pylab.float16)
+      nyeyep, = unpack('I', fin)
+      yeye[trl] = pylab.array(unpack(fmt*nyeyep, fin), dtype=pylab.float16)
 
-    for n in xrange(9):
-      npts, = unpack('I', fin)
-      other_analog_data[trl][n] = pylab.array(unpack('f'*npts, fin), dtype=pylab.float16)
+      nxjoyp, = unpack('I', fin)
+      xjoy[trl] = pylab.array(unpack(fmt*nxjoyp, fin), dtype=pylab.float16)
+      nyjoyp, = unpack('I', fin)
+      yjoy[trl] = pylab.array(unpack(fmt*nyjoyp, fin), dtype=pylab.float16)
 
-    npts, = unpack('I', fin)
-    photodiode[trl] = pylab.array(unpack('f'*npts, fin), dtype=pylab.float16)
-    rt[trl], = unpack('h', fin)
+      if fv > 2.5:
+        for n in xrange(9):
+          npts, = unpack('I', fin)
+          other_analog_data[trl][n] = pylab.array(unpack('f'*npts, fin), dtype=pylab.float16)
+      if fv >= 1.8:
+        npts, = unpack('I', fin)
+        photodiode[trl] = pylab.array(unpack('f'*npts, fin), dtype=pylab.float16)
 
-    numstat, = unpack('I', fin)
-    osr_status_trl = [None]*numstat
-    osr_time_trl = [0]*numstat
-    osr_data_trl = [None]*numstat
-    for n in xrange(numstat):
-      nb, = unpack('I', fin)
-      osr_status_trl[n] = unpack('B'*nb, fin)
-      osr_time_trl[n], = unpack('I', fin)
-      if any(x>1 for x in osr_status_trl[n]):
-        nf, = unpack('B', fin)
-        osr_data_trl[n] = [None]*nf
-        for fnum in xrange(nf):
-          dc, = unpack('I', fin)
-          osr_data_trl[n][fnum] = unpack('d'*dc, fin)
+      rt[trl], = unpack('h', fin)
 
-    osr_status[trl] = osr_status_trl
-    osr_time[trl] = osr_time_trl
-    osr_data[trl] = osr_data_trl
+    if fv >= 1.9:
+      numstat, = unpack('I', fin)
+      osr_status_trl = [None]*numstat
+      osr_time_trl = [0]*numstat
+      osr_data_trl = [None]*numstat
+      if fv >= 2.00:
+        for n in xrange(numstat):
+          nb, = unpack('I', fin)
+          osr_status_trl[n] = unpack('B'*nb, fin)
+          osr_time_trl[n], = unpack('I', fin)
+          if any(x>1 for x in osr_status_trl[n]):
+            nf, = unpack('B', fin)
+            osr_data_trl[n] = [None]*nf
+            for fnum in xrange(nf):
+              dc, = unpack('I', fin)
+              osr_data_trl[n][fnum] = unpack('d'*dc, fin)
+      else:
+        for n in xrange(numstat):
+          nb, = unpack('I', fin)
+          osr_status_trl[n] = unpack('B'*nb, fin)#This might need to be masked in some way
+          osr_time_trl[n], = unpack('I', fin)
 
-    nrew, = unpack('I', fin)
-    rr_ront[trl] = unpack('I'*nrew, fin)
-    rr_rofft[trl] = unpack('I'*nrew, fin)
+      osr_status[trl] = osr_status_trl
+      osr_time[trl] = osr_time_trl
+      osr_data[trl] = osr_data_trl
 
-    nuv, = unpack('B', fin)
-    usrvars[trl] = ['',[None]*nuv]
-    for n in xrange(nuv):
-      varv = None
-      varn, type = unpack('32sc', fin)
-      if type == 'd':
-        lenv, = unpack('B', fin)
-        varv = unpack('d'*lenv, fin)
-      elif type == 'c':
-        varv = unpack('128s', fin)
-      usrvars[trl][n][0] = varn
-      usrvars[trl][n][1] = varv
+    if fv >= 1.95:
+      nrew, = unpack('I', fin)
+      rr_ront[trl] = unpack('I'*nrew, fin)
+      rr_rofft[trl] = unpack('I'*nrew, fin)
+
+    if fv >= 2.7:
+      nuv, = unpack('B', fin)
+      usrvars[trl] = ['',[None]*nuv]
+      for n in xrange(nuv):
+        varv = None
+        varn, type = unpack('32sc', fin)
+        if type == 'd':
+          lenv, = unpack('B', fin)
+          varv = unpack('d'*lenv, fin)
+        elif type == 'c':
+          varv = unpack('128s', fin)
+        usrvars[trl][n][0] = varn
+        usrvars[trl][n][1] = varv
 
   bhv['TrialNumber'] = tno
   bhv['AbsoluteTrialStartTime'] = atst
@@ -295,17 +337,19 @@ def read_footer(bhv, fin):
     nbcu, = unpack('H', fin)
     bhv['CodeNumbersUsed'] = unpack('H'*nbcu, fin)
     bhv['CodeNamesUsed'] = unpack('64s'*nbcu, fin)
-    numf, = unpack('H', fin)
-    vc = {}
-    for n in xrange(numf):
-      fn, cnt = unpack('64sH', fin)
-      vc[fn] = {
-        'Trial': unpack('H'*cnt, fin),
-        'Value': unpack('d'*cnt, fin)
-      }
-    bhv['VariableChanges'] = vc
-    bhv['FinishTime'] = unpack('32s', fin)
 
+    if bhv['FileVersion'] > 2.05:
+      numf, = unpack('H', fin)
+      vc = {}
+      for n in xrange(numf):
+        fn, cnt = unpack('64sH', fin)
+        vc[fn] = {
+          'Trial': unpack('H'*cnt, fin),
+          'Value': unpack('d'*cnt, fin)
+        }
+      bhv['VariableChanges'] = vc
+
+    bhv['FinishTime'] = unpack('32s', fin)
     logger.info('Finished reading file (' + str(fin.tell()) + ' bytes)')
     return True
   except:
@@ -321,23 +365,11 @@ def read_bhv(fname = '../SampleData/WMHU-MJT-06-04-2012.bhv'):
   Output:
     bhv - dictionary with the behavioral data
   """
-
   logger.info('Opening ' + fname)
   with open(fname, "rb") as fin:
     bhv = {}
-    try:
-      read_header(bhv, fin)
-      try:
-        read_trials(bhv, fin)
-        try:
-          read_footer(bhv, fin)
-        except:
-          logger.error('Error reading file footer. File may have terminated abruptly')
-        return bhv
-      except:
-        logger.error('Error reading file body. Aborting')
-    except:
-      logger.error('Error reading file header. Aborting')
+    if read_header(bhv, fin):
+      read_trials(bhv, fin)
+      read_footer(bhv, fin)
 
-  logger.error('Some issue with loading file')
-  return None
+  return bhv
