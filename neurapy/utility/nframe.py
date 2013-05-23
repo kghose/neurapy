e8ff6fa4a1a725fcbbe278ab2829f92500850560
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-05-23 10:11:48 -0400

    Improving docs

--- neurapy/utility/nframe.py
+++ neurapy/utility/nframe.py
@@ -1,25 +1,56 @@
 """
 nframe
 
-In behaving neurophysiological studies we generally present the subject with trials where several events occur, and we are interested in neural responses that occur within some time window relative to those events.
+In behaving neurophysiological studies we generally present the subject with trials where several events occur, and we
+are often interested in neural responses that occur within some time window relative to those events.
 
-nframe is a framework for processing such data. nframe is based on the pandas DataFrame object. It simply encourages you to organize your data in the form of a two dimensional table (described below) and offers different methods for processing the data.
+nframe is a framework for processing such data, built around the pandas DataFrame object. Pandas was chosen for
+* Convenient column and row indexing, including hierarchical indexes and tab completion in ipython (makes it easy to do
+interactive data exploration)
+* Convenient saving to disk
+* Easy interoperability with numpy
 
-nframe expects you to organize your behavioral data into a 2d table (a DataFrame) where each row is a trial and each column represents the time of a particular behavioral epoch you are interested in. If you then point nframe to a file containing spike time stamps it will read in the time stamps and chop them up into time bins and put them in a new DataFrame containing the neural data (spike counts)
+nframe simply encourages you to organize your data in two dimensional tables
+
+Session dataframe
+---------------
+
+Each row is a trial and each column corresponds to some trial parameter or event time.
+
+In addition, when you have neural data, the spike times corresponding to each trial are stored as a numpy array under
+columns corresponding to the neuron identity, eg. n1, n2 etc.
+
+e.g.
+index  | trial_no | correct | stimulus | on_time | off_time | n1              |n2           |
+---------------------------------------------------------------------------------------------
+ ...   |    1     |   0     |   s1     | .1      |    .3    | [23,45,67 ...]  | [.....]
+
+Here
+- index is an index we invent, possibly adding a sesssion code to distinguish this file from others we have.
+- trial_no, correct and stimulus are parameters from our trial that we are interested in.
+- on_time and off_time are timestamps corresponding to events we are interested in
+- n1,n2 are neural data.
+  - These can be spike times or continuous data like LFPs
+  - If data do not exist for a trial (the session can run longer than there is neural data) then we should fill with a Null
+
+
+In general it is advantageous to name the columns starting with an alphabet since that permits tab completion in Ipython
+
 """
 import pandas as pd, pylab, multiprocessing as mp
 import logging
 logger = logging.getLogger(__name__)
 
-def epoch_spike_count(df, nnames=[], epochs=[], bin_edges=[]):
+def epoch_spike_count(df, nnames=[], epochs=[], epoch_names=[], bin_edges=[]):
   """
   This function chops up the spike train into bins around the chosen epochs and saves the spike counts.
 
   Inputs:
-    df          - the trials DataFrame e.g. trials = session_store['trials']
+    df          - a session data frame
     nnames      - names of the neurons (list).
     epochs      - names of the columns (these columns will be treated as containing time stamps for the event)
-    bin_edges   - list of arrays. Each array is a set of bin edges that demarcates the
+    epoch_names - names of the epochs for the binned data
+    bin_edges   - list of arrays. Each array is a set of bin edges that demarcates the bins
 
   The function expects that df[nnames[0]] will return an array containing spike timestamps and df[epochs[0]] will return a single number that tells us when the event in epochs[0] occured.
 
@@ -27,19 +58,33 @@ def epoch_spike_count(df, nnames=[], epochs=[], bin_edges=[]):
     DataFrame with hierarchical column names.
       The column names are (neuronname, epoch, 0) (neuronname, epoch, 1) etc.
 
-  e.g
+    index  | n1                          |n2                             |
+           | e1           |e2            |e1              |e2            |
+           | 0 | 1 ....   | 0 | 1 ...    | 0 | 1 ....                    |
+    ----------------------------------------------------------------------
 
-  run loadsess.py -f='2013-03-11_15-59-25'
-  sc_df = nframe.epoch_spike_count(trials, nnames=['n130311s3c1u1'], epochs=['fixstart','s1on'], bin_edges=[pylab.arange(-.2,.9,.01)]*2)
-  pylab.plot(sc_df.n130311s3c1u1.fixstart[trials.trial_type==1].mean())
-  pylab.plot(sc_df.n130311s3c1u1.fixstart[trials.trial_type==2].mean())
+
+    This structure simply makes it more convenient to handle the data for epoched based analysis, since we now have the spikes
+    organized as 2D arrays, each element of which is an integer [0,1,2...] indicating how many spikes are present in that bin
+    for that trial.
+
+    It also makes accessing epoch based data very convenient, e.g
+
+    df.n1.e2  will return us a n x m array (n trials x m time slices). The peri-event spike count plot is simply
+    pylab.plot(df.n1.e2.sum())
+
+
+    run loadsess.py -f='2013-03-11_15-59-25'
+    sc_df = nframe.epoch_spike_count(trials, nnames=['n130311s3c1u1'], epochs=['fixstart','s1on'], bin_edges=[pylab.arange(-.2,.9,.01)]*2)
+    pylab.plot(sc_df.n130311s3c1u1.fixstart[trials.trial_type==1].mean())
+    pylab.plot(sc_df.n130311s3c1u1.fixstart[trials.trial_type==2].mean())
   """
   sc_df = []
   for nrn in nnames:
     logger.debug('Processing {:s}'.format(nrn))
     if nrn in df.columns:
       ts = df[nrn]#We expect this column to contain spike timestamps
-      for epoch,be in zip(epochs, bin_edges):
+      for epoch,epoch_name,be in zip(epochs, epoch_names, bin_edges):
         this_ts = ts - df[epoch]
         sc = pylab.array([[pylab.nan]*(be.size-1)]*len(df.index))
         #If a row is null then it means that there is no spike data for that trial. This does not mean the neuron did not spike during that period, it means that we did not hold the neuron during that period
@@ -48,7 +93,7 @@ def epoch_spike_count(df, nnames=[], epochs=[], bin_edges=[]):
           if notnulls[n]:
             for m in xrange(be.size-1):
               sc[n,m] = pylab.find((be[m] <= this_ts[n]) & (this_ts[n] < be[m+1])).size
-        col_tuples = [(nrn, epoch, n) for n in xrange(be.size-1)]
+        col_tuples = [(nrn, epoch_name, n) for n in xrange(be.size-1)]
         col_index = pd.MultiIndex.from_tuples(col_tuples, names=['neuron', 'epoch', 'bin'])
         sc_df.append(pd.DataFrame(sc, columns=col_index, index=df.index))
     else:
